180f5cfee4bd845e094d327ef1031c43999dd13d
Evoluwilll <142166104+Evoluwilll@users.noreply.github.com>
2024-04-21 14:12:35 -0300

    aprendendo mais sobre javaScript

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,10 +1,31 @@
-let numeroSecreto = 5;
-console.log(numeroSecreto)
+alert('Boas vindas ao jogo do número secreto');
+let numeroMaximo = 1000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-let chute = prompt('Escolha um número entre 1 e 30');
-
-if (chute == numeroSecreto) {
-    alert('Isso aí! vc descobiru o número secreto (5)');
-} else {
-    alert('Você errou!')
+while (chute != numeroSecreto) {
+   chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+   if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`o número secreto é menor que ${chute}`);
+        } else {
+            alert(`o número secreto é maior que ${chute}`);
+        }
+        tentativas++;
+    }
 }
+
+let palavreTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso aí! vc descobiru o número secreto ${numeroSecreto} com ${tentativas} ${palavreTentativa}`);
+
+
+
+//if(tentativas > 1){
+//   alert(`Isso aí! vc descobiru o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+//}else {
+//  alert(`Isso aí! vc descobiru o número secreto ${numeroSecreto} com ${tentativas} /tentativa.`);
+//}
